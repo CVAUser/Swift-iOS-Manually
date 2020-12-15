3fc87b1227afc17a5d667bc46a71e8943c63251d
CVAUser <57252726+CVAUser@users.noreply.github.com>
2020-12-15 21:49:16 +0300

    init project LayoutAnchors
    Delete storybord, ViewController and not used methods

---
+++ Layout/LayoutAnchors/LayoutAnchors/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  LayoutAnchors
+//
+//  Created by Slava on 15.12.2020.
+//  Copyright © 2020 Slava. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
