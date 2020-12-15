fc30a79f5d9bd05f77bffd75a2d1b26577a24103
CVAUser <57252726+CVAUser@users.noreply.github.com>
2020-12-16 00:51:47 +0300

    override property

--- Layout/LayoutAnchors/LayoutAnchors/AppDelegate.swift
+++ Layout/LayoutAnchors/LayoutAnchors/AppDelegate.swift
@@ -12,15 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let viewController = UIViewController()
+    let redView = UIView()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow.init(frame: UIScreen.main.bounds)
         if let window = self.window {
-            window.rootViewController = UIViewController()
-            window.rootViewController!.view.backgroundColor = .white
+            window.rootViewController = viewController
             window.makeKeyAndVisible()
+            
+            let rootView = viewController.view!
+            rootView.backgroundColor = .white
+            
+            
+            
         }
         return true
     }
