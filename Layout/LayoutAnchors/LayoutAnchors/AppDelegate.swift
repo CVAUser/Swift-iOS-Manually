a0b789bd32c12278d15d9f41805a3dd7ea8ebe3a
Slava <cherkasyuk.slava@gmail.com>
2020-12-16 18:05:56 +0300

    red view layout customize

--- Layout/LayoutAnchors/LayoutAnchors/AppDelegate.swift
+++ Layout/LayoutAnchors/LayoutAnchors/AppDelegate.swift
@@ -14,19 +14,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 //    Default viewContriller and views initializer
     private let viewController = UIViewController()
-    private var root: UIView?
     private let red = UIView()
     private let blue = UIView()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window = UIWindow(frame: UIScreen.main.bounds)
         if let window = self.window {
             window.rootViewController = viewController
             window.makeKeyAndVisible()
-            
         }
+//        Short note for accessing the controller view
+//        Сonstant in local scope
+        let view = viewController.view!
+        view.backgroundColor = .white
+        
+//        Adding views to the controller hierarchy
+        view.addSubview(red)
+        red.backgroundColor = .red
+//        Autoresizing mask dont translated into Auto Layout constraints.
+        red.translatesAutoresizingMaskIntoConstraints = false
+        
+//        Layout customization
+        red.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        red.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        red.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        red.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
         return true
     }
 }
